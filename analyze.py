b8c5a5ca693b53bc7d9352dcd25f46be53907dc5
hoopskin <hoopskin@gmail.com>
2016-09-29 18:16:34 -0500

    Added Gender, Skimm Rate (and by state), improved person object
    Still having some issues with encoding, Foreign address actually
    contains some bad data as well.

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-import mailbox, html, sys
+import mailbox, html, sys, csv
 from bs4 import BeautifulSoup
 
 class Person(object):
@@ -9,7 +9,37 @@ class Person(object):
 		#print("Name: %s\t\t Location: %s\t\t Skimm'Bassador: %r" % (name, location, isSkimmBassador))
 		self.name = name
 		self.location = location
+		self.city = ""
+		self.state = ""
+		self.foreignAddr = None
 		self.isSkimmBassador = isSkimmBassador
+		self.detCityState()
+		self.gender = self.detGender()
+
+	def detGender(self):
+		try:
+			return nameGenderDict[self.name.split()[0]]
+		except(KeyError):
+			return "Unknown"
+
+	def detCityState(self):
+		splitLoc = self.location.split(",")
+		if len(splitLoc) == 1:
+			if self.location[-2:] in abbrevToState.keys():
+				self.state = self.location[-2:]
+				self.city = self.location[:-2].strip()
+			else:
+				self.state = splitLoc[0].strip()
+		else:
+			self.city = splitLoc[0].strip()
+			self.state = splitLoc[1].strip()
+			if self.state in stateToAbbrev.keys():
+				self.state = stateToAbbrev[self.state]
+
+		if self.state in abbrevToState.keys():
+			self.foreignAddr = False
+		else:
+			self.foreignAddr = True
 
 def update_progress(task, progress):
 	"""Creates / Updates a progress bar."""
@@ -31,8 +61,20 @@ def update_progress(task, progress):
 	sys.stdout.write(text)
 	sys.stdout.flush()
 
+def processGenderFile():
+	global nameGenderDict
+	print("Processing Gender File...")
+	reader = csv.reader(open("nameGender.csv", "rt"))
+	header = next(reader)
+	for row in reader:
+		if row[0] in nameGenderDict.keys():
+			nameGenderDict[row[0]] = "Both"
+		else:
+			nameGenderDict[row[0]] = row[1]
+	print("Complete!\n")
+
 def processEmails():
-	global emailCount
+	global emailCount, genderDict
 	mbox = mailbox.mbox('Skimm Emails.mbox')
 
 	i = 1
@@ -43,11 +85,12 @@ def processEmails():
 		msg = item[1]
 		#print(msg['Subject'])
 		if msg['Subject'].startswith("Daily Skimm"):
-			soup = BeautifulSoup(html.unescape(msg.as_string().replace("=2E","",10000).replace("=","",10000).replace("\n","",10000)), "html.parser")
+			soup = BeautifulSoup(html.unescape(msg.as_string().replace("=2E","",10000).replace("=","",10000).replace("\n","",10000)).replace("\xa0",", ", 10000), "html.parser")
 
 			birthdays = soup.find_all('p')[-1].get_text()
 			birthdaysList = birthdays.split(";")
 			for person in birthdaysList:
+				person = person.replace("\xa0"," ", 10000)
 
 				isSkimmBassador = (person.strip()[0]=="*")
 				try:
@@ -58,20 +101,90 @@ def processEmails():
 					location = ""
 				personList.append(Person(name, location, isSkimmBassador))
 
-	#TODO: Standardize location to city, state.
-	#NOTE: There are global individuals.
+
+def preProcess():
+	global locationDict, genderDict, globalSkimmbassadorCount, locationSkimmDict
+
 	for person in personList:
-		if len(person.location) == 0:
-			continue
+		#Location
+		if person.foreignAddr:
+			locationDict["Foreign"]+=1
 		else:
 			try:
-				locationDict[person.location.split(",")[-1].strip()]+=1
+				locationDict[person.state]+=1
 			except(KeyError):
-				locationDict[person.location.split(",")[-1].strip()] = 1
+				locationDict[person.state] = 1
+
+		#Gender
+		try:
+			genderDict[person.gender]+=1
+		except(KeyError):
+			genderDict[person.gender] = 1
+
+		#Global SkimmBassador Rate
+		if person.isSkimmBassador:
+			globalSkimmbassadorCount+=1
+
+		#Location SkimmBassador
+		if person.foreignAddr:
+			locationSkimmDict["Foreign"][1]+=1
+			if person.isSkimmBassador:
+				locationSkimmDict["Foreign"][0]+=1
+		else:
+			try:
+				locationSkimmDict[person.state][1]+=1
+			except(KeyError):
+				locationSkimmDict[person.state] = [0,1]
+
+			#Don't need to do try-catch since previous captures all
+			if person.isSkimmBassador:
+				locationSkimmDict[person.state][0]+=1
+
+
+def printLocations():
+	print("------Location-----")
+
+	locKeys = list(locationDict.keys())
+	locKeys.sort()
+
+	for k in locKeys:
+		print("%s\t%i" % (k, locationDict[k]))
+	
+	print("--End of Location--")
+
+def printGenderSplit():
+	print("------Gender-----")
+
+	for k in list(genderDict.keys()):
+		print("%s\t%i" % (k, genderDict[k]))
+	
+	print("--End of Gender--")
+
+def printSkimmbassadorRate():
+	print("-----Skimmbassador Rate-----")
+	print("%i/%i = %.2f%%" % (globalSkimmbassadorCount, len(personList), globalSkimmbassadorCount/len(personList)*100))
+	print("--End of Simmbassador Rate--")
+
+def printLocSkimmbassadorRates():
+	print("------Skimmbassadors by State-----")
+
+	locKeys = list(locationSkimmDict.keys())
+	locKeys.sort()
+
+	for k in locKeys:
+		if locationSkimmDict[k][1]>0:
+			numerator = locationSkimmDict[k][0]
+			denominator = locationSkimmDict[k][1]
+			print("%s\t%i/%i = %.2f%%\t%i/%i = %.2f%%" % (k, numerator, denominator, numerator/denominator*100, numerator, globalSkimmbassadorCount, numerator/globalSkimmbassadorCount*100))
+	print("--End of Skimmbassadors by State--")
 
 def printResults():
 	print("Found %i individuals across %i emails (weekdays):" % (len(personList), emailCount))
-	print(locationDict)
+	preProcess()
+	printLocations()
+	printGenderSplit()
+	printSkimmbassadorRate()
+	printLocSkimmbassadorRates()
 
 
 def main():
@@ -79,9 +192,17 @@ def main():
 	printResults()
 
 
-locationDict = {}
-personList = []
-stateAbbreviations = {'AL': 'Alabama','AK': 'Alaska','AZ': 'Arizona','AR': 'Arkansas','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','HI': 'Hawaii','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','IA': 'Iowa','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','ME': 'Maine','MD': 'Maryland','MA': 'Massachusetts','MI': 'Michigan','MN': 'Minnesota','MS': 'Mississippi','MO': 'Missouri','MT': 'Montana','NE': 'Nebraska','NV': 'Nevada','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NY': 'New York','NC': 'North Carolina','ND': 'North Dakota','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VT': 'Vermont','VA': 'Virginia','WA': 'Washington','WV': 'West Virginia','WI': 'Wisconsin','WY': 'Wyoming'}
-emailCount = 0
+locationDict = {"Foreign":0}
+nameGenderDict = {}
+genderDict = {}
+locationSkimmDict = {"Foreign":[0,0]}
+abbrevToState = {'AL': 'Alabama','AK': 'Alaska','AZ': 'Arizona','AR': 'Arkansas','CA': 'California','CO': 'Colorado','CT': 'Connecticut','DE': 'Delaware','FL': 'Florida','GA': 'Georgia','HI': 'Hawaii','ID': 'Idaho','IL': 'Illinois','IN': 'Indiana','IA': 'Iowa','KS': 'Kansas','KY': 'Kentucky','LA': 'Louisiana','ME': 'Maine','MD': 'Maryland','MA': 'Massachusetts','MI': 'Michigan','MN': 'Minnesota','MS': 'Mississippi','MO': 'Missouri','MT': 'Montana','NE': 'Nebraska','NV': 'Nevada','NH': 'New Hampshire','NJ': 'New Jersey','NM': 'New Mexico','NY': 'New York','NC': 'North Carolina','ND': 'North Dakota','OH': 'Ohio','OK': 'Oklahoma','OR': 'Oregon','PA': 'Pennsylvania','RI': 'Rhode Island','SC': 'South Carolina','SD': 'South Dakota','TN': 'Tennessee','TX': 'Texas','UT': 'Utah','VT': 'Vermont','VA': 'Virginia','WA': 'Washington','WV': 'West Virginia','WI': 'Wisconsin','WY': 'Wyoming'}
+stateToAbbrev = {'Alabama':'AL','Alaska':'AK','Arizona':'AZ','Arkansas':'AR','California':'CA','Colorado':'CO','Connecticut':'CT','Delaware':'DE','Florida':'FL','Georgia':'GA','Hawaii':'HI','Idaho':'ID','Illinois':'IL','Indiana':'IN','Iowa':'IA','Kansas':'KS','Kentucky':'KY','Louisiana':'LA','Maine':'ME','Maryland':'MD','Massachusetts':'MA','Michigan':'MI','Minnesota':'MN','Mississippi':'MS','Missouri':'MO','Montana':'MT','Nebraska':'NE','Nevada':'NV','New Hampshire':'NH','New Jersey':'NJ','New Mexico':'NM','New York':'NY','North Carolina':'NC','North Dakota':'ND','Ohio':'OH','Oklahoma':'OK','Oregon':'OR','Pennsylvania':'PA','Rhode Island':'RI','South Carolina':'SC','South Dakota':'SD','Tennessee':'TN','Texas':'TX','Utah':'UT','Vermont':'VT','Virginia':'VA','Washington':'WA','West Virginia':'WV','Wisconsin':'WI','Wyoming':'WY'}
 
+personList = []
+
+emailCount = 0
+globalSkimmbassadorCount = 0
+
+processGenderFile()
 main()
