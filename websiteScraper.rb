f7af6299a8f029f165a778de73e64d47d8be9a3c
hoopskin <hoopskin@gmail.com>
2016-10-19 18:14:28 -0500

    Website scrape now grabs all links and goes through them
    I have a sinking suspicion there will just have to be some days that
    need to be added manually (the No Text Weekend one comes to find as a
    very different format than normally expected.

--- websiteScraper.rb
+++ websiteScraper.rb
@@ -1,25 +1,59 @@
 require 'nokogiri'
 require 'net/http'
 
-url = "http://theskimm.com/2016/09/05/skimm-for-september-6th-3"
+#Go to the archive, get all of the links
 
-url = url[url.index("//")+2..-1]
+url = "theskimm.com/archive"
 
 site = Net::HTTP.get(url[0..url.index("/")-1], url[url.index("/")..-1])
 
 pageObj = Nokogiri::HTML(site)
 
-bdays = pageObj.css("p.skimm-p")[-1].css("span").css("span")[0].children
+links = pageObj.css("div.links").css("a")
+i = 0
 
-rawBirthdayText = ""
+suffixes = []
+while i < links.size
+	link = links[i]
+	i+=1
 
-for b in bdays
-	rawBirthdayText = rawBirthdayText + b.text
+	suffixes.insert(suffixes.size, link.attribute('href').value)
 end
 
-for person in rawBirthdayText.split(";")
-	person = person.gsub("\u{a0}", "").gsub("^ ", "").strip.dump
-	puts person[1..-2]
+puts suffixes
+
+i = 0
+#while i < suffixes.size
+for suffix in suffixes
+	#suffix = suffixes[i]
+	i+=1
+	puts suffix
+	url = "http://theskimm.com"+suffix
+	
+	url = url[url.index("//")+2..-1]
+
+	site = Net::HTTP.get(url[0..url.index("/")-1], url[url.index("/")..-1])
+
+	pageObj = Nokogiri::HTML(site)
+
+	begin
+		bdays = pageObj.css("p.skimm-p")[-1].css("span").css("span")[0].children
+	rescue NoMethodError
+		bdays = pageObj.css("p.skimm-p")[-1].children
+	end
+
+	rawBirthdayText = ""
+
+	for b in bdays
+		rawBirthdayText = rawBirthdayText + b.text
+	end
+
+	for person in rawBirthdayText.split(";")
+		person = person.gsub("\u{a0}", "").gsub("^ ", "").strip.dump
+		puts person[1..-2]
+	end
+
+	puts rawBirthdayText.split(";").size.to_s+" birthdays for "+suffix
 end
 
-puts rawBirthdayText.split(";").size.to_s+" birthdays"
+url = "http://theskimm.com/2016/09/05/skimm-for-september-6th-3"
